bb317979661666649f697241e9b7c61e059416dd
Ethan Sagin <ethansagin@gmail.com>
2019-05-03 18:54:11 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,11 +8,7 @@ class Project
   
   def add_backer(backer_inst)
     @backers << backer_inst
-<<<<<<< HEAD
     backer_inst.back_project(self) unless backer_inst.backed_projects.include?(self)
-=======
-    backer_inst.back_project(self)
->>>>>>> c65cbf4843e9d6505ef170306995e2580bb5a889
   end
   
 end
